fd180e5c47a744f2cb93a6faa114008e06375472
MUNAH10 <munahassan5075@gmail.com>
2023-03-23 12:33:50 +0300

    Auth complete

--- api/app/models/user.rb
+++ api/app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
     has_secure_password
     
+    
     validates :Username, {length: {minimum: 5, maximum:8},uniqueness: true, presence: true}
 
     validates :email, {uniqueness: true, presence: true}
